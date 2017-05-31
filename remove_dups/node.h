1a5651acb1f93ecde3f00987760aac9d1b14bf50
Kory Sasaoka <kory.sasaoka@gmail.com>
2017-05-31 08:53:27 -0400

    started implementation of program that removes duplicate nodes in a linked list

---
+++ remove_dups/node.h
@@ -0,0 +1,14 @@
+/**
+ * node in a linked list
+ */
+#ifndef NODE_H
+#define NODE_H
+
+typedef struct node
+{
+    int data;
+    struct node *next;
+}
+node;
+
+#endif
---
+++ remove_dups/remove_dups.c
@@ -0,0 +1,36 @@
+/**
+ * Program that remvoes duplicates from an unsorted linked list
+ */
+
+#include <stdio.h>
+#include "node.h"
+
+void remove(node *head);
+
+int main(void)
+{
+    // create arrays to be turned into linked lists
+    int a1[] = { 2, 7, 6, 2, 4, 2, 8, 9, 1  };
+    int a1[] = { 1, 2, 4, 5, 6, 7, 8, 8, 10 };
+
+    // print arrays
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ", a1[i]);
+        printf("\n");
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ", a2[i]);
+        printf("\n");
+    }
+
+    // create linked lists
+    
+}
+
+void remove(node *head)
+{
+
+}
