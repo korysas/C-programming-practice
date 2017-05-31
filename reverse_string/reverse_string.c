be3509298f856e96cc9c8a29f1800bceae4702be
Kory Sasaoka <kory.sasaoka@gmail.com>
2017-05-30 22:47:36 -0400

    implemented program to reverse a string in place

---
+++ reverse_string/reverse_string.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverse(char *str);
+
+int main(int argc, char *argv[])
+{
+    // ensure correct usage
+    if (argc != 2)
+    {
+        printf("Usage: ./reverse_string str");
+        return 1;
+    }
+
+    reverse(argv[1]);
+
+    printf("%s\n", argv[1]);
+}
+
+void reverse(char *str)
+{
+    int len = strlen(str);
+
+    int j;
+    char temp;
+    for (int i = 0; i < len / 2; i++)
+    {
+        j = len - i - 1;
+
+        // make the swap
+        temp = *(str + i);
+        *(str + i) = *(str + j);
+        *(str + j) = temp;
+    }
+}
