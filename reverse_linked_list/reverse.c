d4a2f4a63680fc0e04c905b5100c6bc4942acc44
Kory Sasaoka <kory.sasaoka@gmail.com>
2017-06-08 15:38:00 -0400

    started implementing program to print a linked list in reverse

---
+++ reverse_linked_list/reverse.c
@@ -0,0 +1,45 @@
+/*
+ * Program that prints a linked list in reverse
+ */
+
+#include <stdio.h>
+
+struct Node
+{
+    int data;
+    struct *Node;
+}
+Node;
+
+// Prototypes
+Node *init_list(int arr[]);
+void dispose_list(Node *node);
+
+int main(void)
+{
+
+}
+
+/*
+ * accepts an integer array and will create a linked list
+ * based on this array
+ *
+ * returns a pointer to the head of the linked list
+ */
+Node *init_list(int arr[])
+{
+
+}
+
+
+/*
+ * calls free() on each Node in the linked list
+ * this function must be called if a list is created with
+ * init_list()
+ *
+ * pass in a pointer to the head of the list
+ */
+void dispose_list(Node *node)
+{
+
+}
